827caadffc7bee350a6f72b3970ad680ba3167db
Ahmad Piran <ahmad.piran@gmail.com>
2025-11-07 21:58:06 +0300

    feat: add main vnet

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = var.virtual_network_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  address_space       = ["10.16.0.0/16"]
+}
--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,11 +1,17 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "myRG"
+  default     = "multitier-infra-rg"
 }
 
 variable "resource_group_location" {
   description = "Location of the resource group"
   type        = string
   default     = "francecentral"
+}
+
+variable "virtual_network_name" {
+  description = "Name of the main virtual network"
+  type        = string
+  default     = "vnet-prod-001"
 }
