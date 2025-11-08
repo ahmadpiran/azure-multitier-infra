0bae88273f37f89b8b8487874f888e488648650c
Ahmad Piran <ahmad.piran@gmail.com>
2025-11-08 21:36:14 +0300

    feat: add subnets

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -9,3 +9,87 @@ resource "azurerm_virtual_network" "main" {
   location            = azurerm_resource_group.main.location
   address_space       = ["10.16.0.0/16"]
 }
+
+resource "azurerm_subnet" "web-001" {
+  name                 = "snet-web-001"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.0.0/20"]
+}
+
+resource "azurerm_subnet" "app-001" {
+  name                 = "snet-app-001"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.16.0/20"]
+}
+
+resource "azurerm_subnet" "db-001" {
+  name                 = "snet-db-001"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.32.0/20"]
+}
+
+resource "azurerm_subnet" "rsrv-001" {
+  name                 = "snet-rsrv-001"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.48.0/20"]
+}
+
+resource "azurerm_subnet" "web-002" {
+  name                 = "snet-web-002"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.64.0/20"]
+}
+
+resource "azurerm_subnet" "app-002" {
+  name                 = "snet-app-002"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.80.0/20"]
+}
+
+resource "azurerm_subnet" "db-002" {
+  name                 = "snet-db-002"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.96.0/20"]
+}
+
+resource "azurerm_subnet" "rsrv-002" {
+  name                 = "snet-rsrv-002"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.112.0/20"]
+}
+
+resource "azurerm_subnet" "web-003" {
+  name                 = "snet-web-003"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.128.0/20"]
+}
+
+resource "azurerm_subnet" "app-003" {
+  name                 = "snet-app-003"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.144.0/20"]
+}
+
+resource "azurerm_subnet" "db-003" {
+  name                 = "snet-db-003"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.160.0/20"]
+}
+
+resource "azurerm_subnet" "rsrv-003" {
+  name                 = "snet-rsrv-003"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.16.176.0/20"]
+}
